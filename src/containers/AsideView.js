e76f72e247098f7cec90b90c92e51bbeebd48a0d
Cristian Caratti <cristian.caratti.xx@gmail.com>
2019-10-08 18:47:46 +0100

    working on getting data from API with fetch() and axios

--- src/containers/AsideView.js
+++ src/containers/AsideView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import axios from 'axios';
+import openweathermap from '../api/openweathermap';
 
 //components
 // import LoginArea from "../components/LoginArea";
@@ -11,10 +11,11 @@ class AsideView extends React.Component {
         allTodayData:[
             {todayIcon:'fa-cloud-rain',weekDay:'Sat',dayMonth:'3 Aug',todayDeg:'28',activeCity:'Berlin',activeCountry:'Germany', activeFeel:'32',activeSunset:'20:15'}
         ],
-        lat: null,
-        lon: null,
+        lat: 0,
+        lon: 0,
         errMessage: '',
-        weather:''
+        weather:[],
+        name:''
     }
 
 componentDidMount() {
@@ -31,42 +32,45 @@ componentDidMount() {
           })
       } );
 
-    //   fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&APPID=dcc4ba580205eb4e86efb0c560cc0b95`)
-    //     .then(res => res.json())
-    //     .then((data) => {
-    //       this.setState({ weather: data.name })
-    //     })
-    //     .catch(console.log)
-
-    // add the parameter to API call:
-    // axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}`,{
-    //   headers: {
-    //     Authorization:'Client-ID dcc4ba580205eb4e86efb0c560cc0b95'
-    //   }
-    // }).then(
-    //   response => console.log(response)
-    // )
-
-    fetch('http://api.openweathermap.org/data/2.5/weather?lat=45.050265599999996&lon=10.911743999999999&appid=dcc4ba580205eb4e86efb0c560cc0b95')
+ 
+      // trying to use dynamic data...
+    // const getPosition= (lat,lon)=>{ fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&APPID=dcc4ba580205eb4e86efb0c560cc0b95`)
+    const getPosition= ()=>{fetch('http://api.openweathermap.org/data/2.5/weather?lat=51.6513792&lon=-0.0827392&appid=dcc4ba580205eb4e86efb0c560cc0b95')
         .then(res => res.json())
         .then((data) => {
-          this.setState({ weather: data.name })
+          this.setState({ weather: data.weather, name:data.name })
         })
         .catch(error => console.log('error in fetching and parsing data', error) )
-    
+    }
+    getPosition()
 
-      
+
+    // 2. trying to use axios...
+//     getData = async (lat,lon) => { const response = await openweathermap.get(`/weather?lat=${this.state.lat}&lon=${this.state.lon}&appid=dcc4ba580205eb4e86efb0c560cc0b95`)
+//     .then(
+//     response => {
+//       this.setState({ weather: response.weather, name:response.name })
+//     }
+//   )
+//   .catch(error => {
+//       console.log('Error', error);
+//     });
+//     }
+
 }
 
+
     render(){
-        console.log(this.state.weather);
-        // let weather = this.state.weather;
+        // console.log(this.state.weather);
+        let weather = this.state.weather;
+
         return(
             <div className='asideStyle' >
                 Lat is: {this.state.lat} and long: {this.state.lon} <br/>{this.state.errMessage}
                 <br />
-                {/* {weather.map( item =>(<h2>{item.description}</h2>) )} */}
-                {this.state.weather}
+                {weather.map( item =>(<h2 key={item.id}>{item.main}{item.description}</h2>) )}
+               <br />
+                {this.state.name}
 
                 {/* <LoginArea /> */}
                 <CentralView allTodayData={this.state.allTodayData} />
