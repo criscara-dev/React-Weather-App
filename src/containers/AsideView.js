3f07024b93ad5fb9d1c49d2f4eab2d78b7c16cc3
Cristian Caratti <criscara@Cristians-MacBook-Pro.local>
2019-10-12 18:23:12 +0100

    deleted unused files

--- src/containers/AsideView.js
+++ src/containers/AsideView.js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 
 //components
-// import LoginArea from "../components/LoginArea";
 import CentralView from "../components/CentralView";
 import DayForecast from "../components/DayForecast";
 
